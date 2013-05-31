4bf248c08a88b32b231dda045dc4d4b8a487058b
Emil Wall <emil.wall@valtech.se>
2013-05-31 13:42:32 +0200

    Move AlienBullet class into separate file
    AlienBullet inherits from Sprite, so alienbullet.js depends on sprite.js

--- game.js
+++ game.js
@@ -1,20 +1,3 @@
-AlienBullet = function () {
-  this.init("alienbullet");
-
-  this.draw = function () {
-    if (this.visible) {
-      this.context.save();
-      this.context.lineWidth = 2;
-      this.context.beginPath();
-      this.context.moveTo(this.x, this.y);
-      this.context.lineTo(this.x-this.vel.x, this.y-this.vel.y);
-      this.context.stroke();
-      this.context.restore();
-    }
-  };
-};
-AlienBullet.prototype = new Bullet();
-
 Asteroid = function () {
   this.init("asteroid",
             [-10,   0,
