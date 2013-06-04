d22d7a8765df05572706ca42f372667f294fb2ed
Emil Wall <emil.wall@valtech.se>
2013-06-04 19:50:03 +0200

    Test that preMove can reset rotation velocity

--- sprites-spec/ship-spec.js
+++ sprites-spec/ship-spec.js
@@ -20,4 +20,18 @@ describe("Ship", function() {
       expect(asteroids.Game.lives).toBe(2);
     });
   });
+
+  it("should have preMove method", function() {
+    expect(typeof this.ship.collision).toBe("function");
+  });
+
+  describe("preMove", function() {
+    it("should set rotation velocity to zero when no key is pressed", function() {
+      this.ship.vel.rot = 5;
+
+      this.ship.preMove(3);
+
+      expect(this.ship.vel.rot).toBe(0);
+    });
+  });
 });
