01ab0e8bffdb22435f9b7d5c8aab4ed0fbc4a397
Emil Wall <emil.wall@valtech.se>
2013-06-04 18:47:42 +0200

    Add KEY_CODES to asteroids namespace

--- core/config.js
+++ core/config.js
@@ -1,4 +1,6 @@
-KEY_CODES = {
+var asteroids = asteroids || {};
+
+asteroids.KEY_CODES = {
   32: 'space',
   37: 'left',
   38: 'up',
@@ -8,24 +10,24 @@ KEY_CODES = {
   71: 'g',
   72: 'h',
   80: 'p'
-}
+};
 
 KEY_STATUS = { keyDown:false };
-for (code in KEY_CODES) {
-  KEY_STATUS[KEY_CODES[code]] = false;
-}
+for (code in asteroids.KEY_CODES) {
+  KEY_STATUS[asteroids.KEY_CODES[code]] = false;
+};
 
 $(window).keydown(function (e) {
   KEY_STATUS.keyDown = true;
-  if (KEY_CODES[e.keyCode]) {
+  if (asteroids.KEY_CODES[e.keyCode]) {
     e.preventDefault();
-    KEY_STATUS[KEY_CODES[e.keyCode]] = true;
+    KEY_STATUS[asteroids.KEY_CODES[e.keyCode]] = true;
   }
 }).keyup(function (e) {
   KEY_STATUS.keyDown = false;
-  if (KEY_CODES[e.keyCode]) {
+  if (asteroids.KEY_CODES[e.keyCode]) {
     e.preventDefault();
-    KEY_STATUS[KEY_CODES[e.keyCode]] = false;
+    KEY_STATUS[asteroids.KEY_CODES[e.keyCode]] = false;
   }
 });
 
