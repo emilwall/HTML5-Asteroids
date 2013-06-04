4d8684de8a19dbfb1eb485ff0ef969bbd57affbe
Emil Wall <emil.wall@valtech.se>
2013-06-04 10:51:04 +0200

    Add test specification comment for config

---
+++ core-spec/config-spec.js
@@ -0,0 +1,7 @@
+// Defines KEY_CODES object with mappings from key numbers to logical names
+// Defines KEY_STATUS object with keyDown attribute and a boolean for each key code
+/* Sets keydown and keyup events on window using jQuery:
+ * Always sets keyDown attribute of KEY_STATUS
+ * If defined as key_code, preventDefault of the event and set KEY_STATUS
+ */
+// Defines GRID_SIZE (to 60)
