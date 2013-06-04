14678d6743b3aa4b9f18e4c6a8c1381fdaa939f9
Emil Wall <emil.wall@valtech.se>
2013-06-04 12:46:33 +0200

    Add tests for explosion class

--- sprites-spec/explosion-spec.js
+++ sprites-spec/explosion-spec.js
@@ -1,4 +1,20 @@
 // init: set name to explosion
+describe("Explosion", function() {
+  beforeEach(function() {
+    this.explosion = new Explosion();
+  });
+
+  it("should have init method", function() {
+    expect(typeof this.explosion.init).toBe("function");
+  });
+
+  describe("init", function() {
+    it("should set name to explosion", function() {
+      expect(this.explosion.name).toBe("explosion");
+    });
+  });
+});
+
 // bridgesH, bridgesV = false
 // lines should be of length 5 and contain arrays of length 4: [x1, y1, x2, y2]
 /* draw:
