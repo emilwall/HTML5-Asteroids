e57300a1786e1625452ddce35ff56e349668a906
Emil Wall <emil.wall@valtech.se>
2013-06-04 10:40:28 +0200

    Add test specification comments for explosion

---
+++ sprites-spec/explosion-spec.js
@@ -0,0 +1,10 @@
+// init: set name to explosion
+// bridgesH, bridgesV = false
+// lines should be of length 5 and contain arrays of length 4: [x1, y1, x2, y2]
+/* draw:
+ * do nothing if not visible
+ * draw lines using beginPath, moveTo, lineTo and stroke
+ * save and restore context
+ */
+// preMove: Increase scale, die if scale exceeds limit (8).
+// Inherits from Sprite
