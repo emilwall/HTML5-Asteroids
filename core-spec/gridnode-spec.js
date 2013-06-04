ec66fdd8cdc5d727a8445e69826a5ac3501ca8c4
Emil Wall <emil.wall@valtech.se>
2013-06-04 11:02:43 +0200

    Add test specification comment for gridnode

---
+++ core-spec/gridnode-spec.js
@@ -0,0 +1,6 @@
+// Initialize north, south, east, west, nextSprite to null (or at least something not undefined)
+// Initialize dupe as object with null horizontal and vertical attributes
+// enter: add sprite to grid node
+// leave: remove sprite from grid node
+// eachSprite: call the callback on every sprite in grid node
+// isEmpty: checks whether grid node contains any sprites with name in collidables
