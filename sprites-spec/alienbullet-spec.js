f4dc1bd0572bf5957392562e13fb0bf40f6d9bac
Emil Wall <emil.wall@valtech.se>
2013-06-11 22:30:23 +0200

    Test that the init function is defined as AlienBullet method

--- sprites-spec/alienbullet-spec.js
+++ sprites-spec/alienbullet-spec.js
@@ -1,7 +1,17 @@
-// init: set name of object
-/* draw:
- * do nothing if not visible
- * call save and then restore of context
- * draw line on canvas
- */
-// Inherits from bullet
+describe("AlienBullet", function() {
+  beforeEach(function () {
+    this.alienBullet = new AlienBullet();
+  });
+
+  it("should have init method", function() {
+    expect(typeof this.alienBullet.init).toBe("function");
+  });
+
+  // init: set name of object
+  /* draw:
+   * do nothing if not visible
+   * call save and then restore of context
+   * draw line on canvas
+   */
+  // Inherits from bullet
+});
