c7521ebf4775caef458a1bd9fa5099ffabfb7ab9
Emil Wall <emil.wall@valtech.se>
2013-06-21 12:01:07 +0200

    Use local var instead of using 'this'

--- core-spec/gridnode-spec.js
+++ core-spec/gridnode-spec.js
@@ -1,13 +1,15 @@
 describe("GridNode", function () {
+  var gridNode;
+
   beforeEach(function () {
-    this.gridNode = new asteroids.GridNode();
+    gridNode = new asteroids.GridNode();
   });
 
   it("should have enter, leave, eachSprite and isEmpty methods", function () {
-    expect(typeof this.gridNode.enter).toEqual("function");
-    expect(typeof this.gridNode.leave).toEqual("function");
-    expect(typeof this.gridNode.eachSprite).toEqual("function");
-    expect(typeof this.gridNode.isEmpty).toEqual("function");
+    expect(typeof gridNode.enter).toEqual("function");
+    expect(typeof gridNode.leave).toEqual("function");
+    expect(typeof gridNode.eachSprite).toEqual("function");
+    expect(typeof gridNode.isEmpty).toEqual("function");
   });
   // Initialize north, south, east, west, nextSprite to null (or at least something not undefined)
   // Initialize dupe as object with null horizontal and vertical attributes
