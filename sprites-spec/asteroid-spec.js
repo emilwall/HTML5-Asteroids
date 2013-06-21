e6cc3db3279e09e7dcaa1964843e1d0280163957
Emil Wall <emil.wall@valtech.se>
2013-06-21 12:00:10 +0200

    Use local vars instead of using 'this'

--- sprites-spec/asteroid-spec.js
+++ sprites-spec/asteroid-spec.js
@@ -1,27 +1,28 @@
 describe("Asteroid", function () {
+  var asteroid, gameScore;
+
   beforeEach(function () {
-    this.asteroid = new asteroids.Asteroid();
+    asteroid = new asteroids.Asteroid();
     sinon.stub(asteroids.Game, "addSprite");
-    this.gameScore = asteroids.Game.score;
+    gameScore = asteroids.Game.score;
   });
 
   afterEach(function () {
     asteroids.Game.addSprite.restore();
-    asteroids.Game.score = this.gameScore;
+    asteroids.Game.score = gameScore;
   });
 
   describe("Collision", function () {
-
     beforeEach(function () {
-      this.asteroid.die = sinon.spy();
+      asteroid.die = sinon.spy();
     });
 
     it("should die when other is bullet", function () {
       var bullet = { name: "bullet" };
 
-      this.asteroid.collision(bullet);
+      asteroid.collision(bullet);
 
-      assert(this.asteroid.die.called);
+      assert(asteroid.die.called);
     });
   });
 });
