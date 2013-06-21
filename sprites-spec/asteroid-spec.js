27fe272270310ff683a5dbc0e174fa2e68709db5
Emil Wall <emil.wall@valtech.se>
2013-06-21 12:08:51 +0200

    Improve stubbing of asteroids.Asteroid spec

--- sprites-spec/asteroid-spec.js
+++ sprites-spec/asteroid-spec.js
@@ -1,20 +1,33 @@
 describe("Asteroid", function () {
-  var asteroid, gameScore;
+  var spriteInit, asteroid, gameScore;
 
   beforeEach(function () {
-    asteroid = new asteroids.Asteroid();
+    spriteInit = sinon.spy();
+    sinon.stub(asteroids, "Sprite").returns({ init: spriteInit });
+    sinon.stub(asteroids.Asteroid.prototype, "init");
     sinon.stub(asteroids.Game, "addSprite");
     gameScore = asteroids.Game.score;
+
+    asteroid = new asteroids.Asteroid();
+    asteroid.vel = {};
   });
 
   afterEach(function () {
+    asteroids.Sprite.restore();
+    asteroids.Asteroid.prototype.init.restore();
     asteroids.Game.addSprite.restore();
     asteroids.Game.score = gameScore;
+  });
+
+  it("should call asteroids.Asteroid.prototype.init in constructor", function () {
+    expect(asteroids.Asteroid.prototype.init.called).toEqual(true);
   });
 
   describe("Collision", function () {
     beforeEach(function () {
       asteroid.die = sinon.spy();
+      asteroid.move = sinon.spy();
+      asteroid.points = asteroids.Asteroid.prototype.init.args[0][1];
     });
 
     it("should die when other is bullet", function () {
