2c48b4e45fb9f86b2ced4b15adb39ffc4fdaf883
Emil Wall <emil.wall@valtech.se>
2013-06-28 14:29:17 +0200

    Refactor AlienBullet tests

--- sprites-spec/alienbullet-spec.js
+++ sprites-spec/alienbullet-spec.js
@@ -1,19 +1,28 @@
 describe("AlienBullet", function () {
-  var alienBullet;
+  var alienBullet, alienBulletPrototype;
 
   beforeEach(function () {
+    alienBulletPrototype = asteroids.AlienBullet.prototype;
+    asteroids.AlienBullet.prototype = { init: sinon.spy() };
+
     alienBullet = new asteroids.AlienBullet();
   });
 
-  it("should have init method", function () {
-    expect(typeof alienBullet.init).toEqual("function");
+  afterEach(function () {
+    asteroids.AlienBullet.prototype = alienBulletPrototype;
   });
 
-  // init: set name of object
+  it("should use implementation of init method from prototype", function () {
+    expect(alienBullet.init).toBe(asteroids.AlienBullet.prototype.init);
+  });
+
+  it("should call init method in constructor", function () {
+    sinon.assert.called(alienBullet.init);
+  });
+
   /* draw:
    * do nothing if not visible
    * call save and then restore of context
    * draw line on canvas
    */
-  // Inherits from bullet
 });
