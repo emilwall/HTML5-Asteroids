c1b0c1e0f72aaf92f5a7e9fe35d825f7be326806
Emil Wall <emil.wall@valtech.se>
2013-06-30 22:05:58 +0200

    Improve tests of Bullet, inspired by AlienBullet tests

--- sprites-spec/bullet-spec.js
+++ sprites-spec/bullet-spec.js
@@ -1,24 +1,63 @@
 describe("Bullet", function () {
-  var bullet;
+  var bullet, bulletPrototype;
 
   beforeEach(function () {
+    var fakeContext = {
+      beginPath: sinon.spy(),
+      closePath: sinon.spy(),
+      moveTo: sinon.spy(),
+      lineTo: sinon.spy(),
+      stroke: sinon.spy(),
+      save: sinon.spy(),
+      restore: sinon.spy()
+    };
+    bulletPrototype = asteroids.Bullet.prototype;
+    asteroids.Bullet.prototype = { init: sinon.spy(), context: fakeContext };
+
     bullet = new asteroids.Bullet();
+    bullet.visible = true;
+    bullet.vel = {};
   });
 
-  it("should have init method", function () {
-    expect(typeof bullet.init).toEqual("function");
+  afterEach(function () {
+    asteroids.Bullet.prototype = bulletPrototype;
   });
 
-  // init: set name (and coordinates (0, 0)?)
+  it("should use implementation of init method from prototype", function () {
+    expect(bullet.init).toBe(asteroids.Bullet.prototype.init);
+  });
+
+  it("should call init method in constructor", function () {
+    sinon.assert.called(bullet.init);
+    sinon.assert.calledWith(bullet.init, "bullet", [0, 0]);
+  });
+
+  describe("draw", function () {
+    it("should do nothing when not visible", function () {
+      bullet.visible = false;
+      bullet.context = null; // would cause exception if the method did anything
+
+      bullet.draw();
+    });
+
+    it("should call save and then restore of context", function () {
+      bullet.draw();
+
+      sinon.assert.callOrder(bullet.context.save, bullet.context.restore);
+    });
+
+    it("should draw on canvas", function () {
+      bullet.draw();
+
+      sinon.assert.called(bullet.context.lineTo);
+      sinon.assert.called(bullet.context.stroke);
+    });
+  });
+
   // does not collide with anything (asteroids and aliens check this themselves)
   // sets time, bridgesH and bridgesV attributes
   // defines postMove to be same as asteroids.Sprite.wrapPostMove
-  // defined configureTransform as empty function (why?)
-  /* draw:
-   * do nothing if not visible
-   * call save and then restore of context
-   * draw square (dot) on canvas
-   */
+  // defines configureTransform as empty function (why?)
   /* preMove:
    * do nothing if not visible
    * set to invisible AND reset time if time > threshold (50)
