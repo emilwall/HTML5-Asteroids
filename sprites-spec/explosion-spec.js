b853cb804967930e6fea5409502d25636c181e0b
Emil Wall <emil.wall@valtech.se>
2013-07-02 19:19:02 +0200

    Add tests for explosion class

--- sprites-spec/explosion-spec.js
+++ sprites-spec/explosion-spec.js
@@ -3,10 +3,20 @@ describe("Explosion", function () {
   var explosion, explosionPrototype;
 
   beforeEach(function () {
+    var fakeContext = {
+      beginPath: sinon.spy(),
+      moveTo: sinon.spy(),
+      lineTo: sinon.spy(),
+      stroke: sinon.spy(),
+      save: sinon.spy(),
+      restore: sinon.spy()
+    };
     explosionPrototype = asteroids.Explosion.prototype;
-    asteroids.Explosion.prototype = { init: sinon.spy() };
+    asteroids.Explosion.prototype = { init: sinon.spy(), context: fakeContext, die: sinon.spy() };
 
     explosion = new asteroids.Explosion();
+    explosion.visible = true;
+    explosion.scale = 2;
   });
 
   afterEach(function () {
@@ -21,14 +31,74 @@ describe("Explosion", function () {
     sinon.assert.called(explosion.init);
     sinon.assert.calledWith(explosion.init, "explosion");
   });
-});
 
-// bridgesH, bridgesV = false
-// lines should be of length 5 and contain arrays of length 4: [x1, y1, x2, y2]
-/* draw:
- * do nothing if not visible
- * draw lines using beginPath, moveTo, lineTo and stroke
- * save and restore context
- */
-// preMove: Increase scale, die if scale exceeds limit (8).
-// Inherits from asteroids.Sprite
+  it("should initialize ridgesH and bridgesV to false", function () {
+    expect(explosion.bridgesH).toEqual(false);
+    expect(explosion.bridgesV).toEqual(false);
+  });
+
+  describe("lines", function () {
+    it("should be of length 5", function () {
+      expect(explosion.lines.length).toEqual(5);
+    });
+
+    it("should contain arrays of length 4: [x1, y1, x2, y2]", function () {
+      expect(explosion.lines[0].length).toEqual(4);
+      expect(explosion.lines[4].length).toEqual(4);
+    });
+  });
+
+  describe("draw", function () {
+    it("should do nothing if not visible", function () {
+      explosion.visible = false;
+
+      explosion.draw();
+
+      sinon.assert.notCalled(explosion.context.save);
+    });
+
+    it("should draw lines using beginPath, moveTo, lineTo and stroke", function () {
+      explosion.draw();
+
+      sinon.assert.called(explosion.context.beginPath);
+      sinon.assert.called(explosion.context.moveTo);
+      sinon.assert.called(explosion.context.lineTo);
+      sinon.assert.called(explosion.context.stroke);
+    });
+
+    it("should save and restore context", function () {
+      explosion.draw();
+
+      sinon.assert.called(explosion.context.save);
+      sinon.assert.called(explosion.context.restore);
+    });
+  });
+
+  describe("preMove", function () {
+    it("should increase scale when visible", function () {
+      explosion.preMove(3);
+
+      expect(explosion.scale).toEqual(5);
+    });
+
+    it("should not increase scale when not visible", function () {
+      explosion.visible = false;
+
+      explosion.preMove(3);
+
+      expect(explosion.scale).toEqual(2);
+    });
+
+    it("should die when scale exceeds limit (8)", function () {
+      explosion.preMove(7);
+
+      sinon.assert.called(explosion.die);
+    });
+
+    it("should not die when scale does not exceed limit (8)", function () {
+      explosion.preMove(6);
+
+      sinon.assert.notCalled(explosion.die);
+    });
+  });
+});
